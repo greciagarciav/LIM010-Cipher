e0453461effb3fbed9e1a7ca7a853a5c2f3fff97
greciagarciav <garciav.grecia@gmail.com>
2019-06-05 07:15:20 -0500

    Agrega validacion de contraseña

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 /* Acá va tu código */
+let intentos = 0;
+
+const btn = document.getElementById('btnContrasena');
+
+btn.addEventListener('click', () =>{
+    let contrasena = document.getElementById('contrasena').value;
+
+    ValidarContrasena(contrasena);
+
+    console.log(contrasena);
+});
+
+function ValidarContrasena(cons){
+    if (cons == 'LABORATORIA') {
+        document.getElementById('cifrar').style.display="none";
+        document.getElementById('resultadocifrado').style.display="block";
+    }
+    else{
+        if(intentos >= 3){
+            alert("Agotaste tus intentos");
+            //Código para bloquear el input
+        }
+        else{
+            alert("La contraseña no es correcta");
+        };
+        intentos++;
+        console.log(intentos);
+    }
+}
