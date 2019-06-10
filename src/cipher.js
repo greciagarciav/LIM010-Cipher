51bbdc151890a07e24161c9dd64335314ab9bbc5
greciagarciav <garciav.grecia@gmail.com>
2019-06-09 23:30:56 -0500

    codifica una letra

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,18 @@
+const asciiInicio = 65;
+const alfabetoRango = 26;
+
 window.cipher = {
    /*Es el objeto*/
   encode: (offset, string) => {
     /* Acá va tu código que cifra*/
     /* offset y string son los parametros*/
    /* Encode es el método*/
+    let ascii = string.charCodeAt(0);
+    let cifrar = (ascii - asciiInicio + offset) % alfabetoRango + asciiInicio;
+    let letraCifrada = String.fromCharCode(cifrar);
+
+      console.log(letraCifrada);
+
   },
   decode: (offset, string) => {
     /* Acá va tu código que descifra*/
